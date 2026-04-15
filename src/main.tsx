c16a0839011ab2a84428305ecee5c7cbdb889c9d
travisholcombe-coder <travis.holcombe@gmail.com>
2026-04-15 03:40:40 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../app/globals.css'
+import './index.css'
 import Home from './Home'
 
 createRoot(document.getElementById('root')!).render(

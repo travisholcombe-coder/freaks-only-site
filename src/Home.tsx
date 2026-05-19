b5e463da29d66b15d06667f707633157917f2218
travisholcombe-coder <travis.holcombe@gmail.com>
2026-05-18 21:52:00 -0700

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,21 +2,21 @@ import { Header } from "@/components/radio/header"
 import { NowPlaying } from "@/components/radio/now-playing"
 import { ContentGrid } from "@/components/radio/content-grid"
 import { AudioPlayer } from "@/components/radio/audio-player"
+import { ThanksBanner } from "@/components/radio/thanks-banner"
 
 export default function Home() {
   return (
     <main className="min-h-screen pb-20">
       {/* Aggressive Header */}
       <Header />
-
+      {/* Thanks Banner (shows after Stripe donation redirect) */}
+      <ThanksBanner />
       {/* Now Playing Ticker */}
       <div className="p-4">
         <NowPlaying />
       </div>
-
       {/* Raw Grid Content */}
       <ContentGrid />
-
       {/* Fixed Audio Player */}
       <AudioPlayer />
     </main>

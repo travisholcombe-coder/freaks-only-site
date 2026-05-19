d2da838bf4ca6138ed10b438efc824d3a2188e2e
travisholcombe-coder <travis.holcombe@gmail.com>
2026-05-18 22:18:58 -0700

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,6 @@
 import { Header } from "@/components/radio/header"
 import { NowPlaying } from "@/components/radio/now-playing"
 import { ContentGrid } from "@/components/radio/content-grid"
-import { AudioPlayer } from "@/components/radio/audio-player"
 import { ThanksBanner } from "@/components/radio/thanks-banner"
 
 export default function Home() {
@@ -17,8 +16,6 @@ export default function Home() {
       </div>
       {/* Raw Grid Content */}
       <ContentGrid />
-      {/* Fixed Audio Player */}
-      <AudioPlayer />
     </main>
   )
 }

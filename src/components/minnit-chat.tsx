aa988b3957fbc47a278701af2917658f529469ec
travisholcombe-coder <travis.holcombe@gmail.com>
2026-06-12 05:03:27 -0700

    Update minnit-chat.tsx

--- src/components/minnit-chat.tsx
+++ src/components/minnit-chat.tsx
@@ -13,7 +13,7 @@ export function MinnitChat() {
         style={{ display: "none" }}
         className="minnit-chat-sembed"
         data-chatname="https://organizations.minnit.chat/562854651725297/c/Main?embed"
-        data-style="width:100%; height:450px;"
+        data-style="width:100%; height:520px;"
         data-version="1.55"
       >
         Chat
